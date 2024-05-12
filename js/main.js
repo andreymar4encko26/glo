ba61291ddf00f88bff820f04f27bc0b4d1bd7367
«andrey» <andrey.mar4encko2018@yandex.ru>
2024-05-12 22:05:02 +0300

    второй урок

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-let title= "";
-let screens ="";
-let screenPrice ="";
-let rollback ="";
-let fullPrice ="";
-let adaptive ="";
+let title= "первый проэкт ";
+let screens ='Простые,Сложные,Интерактивные'.toLowerCase();
+let screenPrice =2000;
+let rollback =30;
+let fullPrice ="40000";
+let adaptive =true;
+let screensArray= screens.split(',')
 
 alert('первый урок');
+
+console.log(typeof(title,fullPrice,adaptive))
+console.log(screens.length)
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/`)
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`)
+console.log(screensArray)
 console.log('первый урок')
+console.log(fullPrice * (rollback/100))
